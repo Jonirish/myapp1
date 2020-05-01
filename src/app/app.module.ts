014f61ddbe5a550cb0cfb8058e8a10ba0135a667
Jonirish <48059655+Jonirish@users.noreply.github.com>
2020-05-01 16:42:42 +0100

    added

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePagePage } from '../pages/home-page/home-page';
+
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePagePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePagePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home-page/home-page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home-page',
+  templateUrl: 'home-page.html'
+})
+export class HomePagePage {
+
+  constructor(public navCtrl: NavController) {
+  }
+  
+}
